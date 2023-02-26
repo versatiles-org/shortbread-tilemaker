3aa249387043693a1b126ce28cbd3ab6a7ae35ab
yetzt <git@yetzt.me>
2023-02-26 22:00:50 +0100

    get `admin_points.shp` from `versatiles/admin.geojson`

--- get-shapefiles.sh
+++ get-shapefiles.sh
@@ -24,9 +24,9 @@ cd "$(dirname "$0")" || exit
 mkdir -p data
 download_and_extract "https://osmdata.openstreetmap.de/download/water-polygons-split-4326.zip" "water-polygons-split-4326.zip"
 download_and_extract "https://osmdata.openstreetmap.de/download/simplified-water-polygons-split-3857.zip" "simplified-water-polygons-split-3857.zip"
-download_and_extract "https://shortbread.geofabrik.de/shapefiles/admin-points-4326.zip" "admin-points-4326.zip"
-mkdir -p data/simplified-water-polygons-split-4326
-transform_shp data/simplified-water-polygons-split-3857/simplified_water_polygons.shp data/simplified-water-polygons-split-4326/simplified_water_polygons.shp
 
 mkdir -p data/admin-points-4326
-ogr2ogr data/admin-points-4326/admin_points.shp versatiles/admin.geojson -lco ENCODING=UTF-8
+ogr2ogr -f "ESRI Shapefile" "data/admin-points-4326/admin_points.shp" "versatiles/admin.geojson" -t_srs EPSG:4326 -lco ENCODING=UTF-8
+
+mkdir -p data/simplified-water-polygons-split-4326
+transform_shp data/simplified-water-polygons-split-3857/simplified_water_polygons.shp data/simplified-water-polygons-split-4326/simplified_water_polygons.shp
